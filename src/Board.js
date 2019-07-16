58a785b42d2f2c5b3c9a0a979baf66e87017dc3f
Aziz Ahmed <azizahmedsabuj@gmail.com>
2019-07-16 18:06:16 +0600

    Formated table view for all players

--- src/Board.js
+++ src/Board.js
@@ -19,17 +19,31 @@ class Board extends Component {
 		this.state = {};
 	}
 
-	renderCard = () => {
-		return this.props.G.playersCards[this.props.playerID].map((card, index) => (
+	renderCard = (playerID) => {
+		return this.props.G.playersCards[playerID].map((card, index) => (
 			<Card key={card.id} name={card.name} move={this.props.moves.passCard} index={index} />
 		));
 	};
 
+	//for display position
+	//current player will sit bottom
+	getPlayerByPosition(position) {
+		let playerID = Number(this.props.playerID);
+
+		return playerID + position <= 3 ? (playerID + position).toString() : '0';
+	}
+
 	render() {
-		console.log(this.props);
+		// console.log(this.props);
+
 		return (
-			<div className="board-container">
-				<div className="card-container">{this.renderCard()}</div>
+			<div className={'board-container column'}>
+				<div className="card-container row">{this.renderCard(this.getPlayerByPosition(2))}</div>
+				<div className="board-container row">
+					<div className="card-container column">{this.renderCard(this.getPlayerByPosition(3))}</div>
+					<div className="card-container column">{this.renderCard(this.getPlayerByPosition(1))}</div>
+				</div>
+				<div className="card-container row">{this.renderCard(this.getPlayerByPosition(0))}</div>
 			</div>
 		);
 	}
